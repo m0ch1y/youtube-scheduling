aeb1652dbd8330212abf432f32bf9cc474949a61
m0ch1y <Kaito.m_317@outlook.jp>
2023-02-27 23:13:58 +0900

    delete api popup

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -8,13 +8,11 @@ function Content({ videos }) {
     return (
       <div>
         {videos.map((detailData, index) => (
-          <Grid
-            key={index}
-            container
-            spacing={4}
-            style={{ overflowX: "scroll" }}
-          >
-            <div style={{ display: "flex", width: "4600px" }}>
+          <Grid key={index} container spacing={4}>
+            <div
+              className="scroll"
+              style={{ display: "flex", width: "4600px" }}
+            >
               {detailData.details.map((video) => (
                 <Grid key={video.id} ml={6} mr={2} mt={8} mb={4} item>
                   <div>
